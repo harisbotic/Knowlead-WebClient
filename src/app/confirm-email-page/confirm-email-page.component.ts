7b9aba26e2ccd9fb248f00f4ee5431f1f88a216d
charlieamer <amer_zavlan@live.com>
2016-09-08 18:56:15 +0200

    email confirm from routes

--- src/app/confirm-email-page/confirm-email-page.component.ts
+++ src/app/confirm-email-page/confirm-email-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 import { ConfirmEmail } from '../models';
 
 @Component({
@@ -7,10 +8,14 @@ import { ConfirmEmail } from '../models';
   styleUrls: ['./confirm-email-page.component.scss']
 })
 export class ConfirmEmailPageComponent implements OnInit {
-  confirm: ConfirmEmail = new ConfirmEmail(); 
-  constructor() { }
+  confirm: ConfirmEmail = new ConfirmEmail();
+  constructor(protected route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params:any) => {
+      this.confirm.code = params.code;
+      this.confirm.email = params.email;
+    })
   }
 
 }
