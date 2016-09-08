029fefca96916c9f89c8c6dc1c64ae5506cc4f1c
Ahmed Karic <ahmeddd_karic@hotmail.com>
2016-09-08 18:54:46 +0200

    confirm i subscribe

--- src/app/confirm-email-page/confirm-email-page.component.ts
+++ src/app/confirm-email-page/confirm-email-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { ConfirmEmail } from '../models';
+import { ConfirmEmail, ErrorModel } from '../models';
+import { SessionService} from '../session.service';
 
 @Component({
   selector: 'app-confirm-email-page',
   templateUrl: './confirm-email-page.component.html',
-  styleUrls: ['./confirm-email-page.component.scss']
+  styleUrls: ['./confirm-email-page.component.scss'],
+
+  providers: [SessionService]
+
 })
 export class ConfirmEmailPageComponent implements OnInit {
-  confirm: ConfirmEmail = new ConfirmEmail(); 
-  constructor() { }
+  confirm: ConfirmEmail = new ConfirmEmail();
+  errors: ErrorModel[];
+  constructor( protected sessionservice: SessionService) { }
 
+  confirmEmail(){
+    this.sessionservice.confirmEmail(this.confirm).subscribe((result) => {
+      
+    },(errorResult)=>{
+      this.errors = errorResult.json().errorList;
+    });
+  }
   ngOnInit() {
   }
 
