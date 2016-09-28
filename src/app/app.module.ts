afe1ad45d44d30e6a55b965149fdd4942769a1f2
charlieamer <amer_zavlan@live.com>
2016-09-28 17:31:39 +0200

    migration to lightning design system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http, RequestOptions, XHRBackend } from '@angular/http';
 
-import { MdButtonModule } from '@angular2-material/button';
-import { MdCardModule } from '@angular2-material/card';
-import { MdInputModule } from '@angular2-material/input';
-import { MdCoreModule } from '@angular2-material/core';
-
 import { AppRouting } from "./app.routing"
 import { AppComponent } from './app.component';
 import { GuestHomePageComponent } from './guest-home-page/guest-home-page.component';
@@ -21,6 +16,8 @@ import { HttpProvider } from './http.provider';
 import { StorageService } from './storage.service';
 import { UserHomePageComponent } from './user-home-page/user-home-page.component';
 import { Router } from '@angular/router';
+import { NglModule } from 'ng-lightning';
+
 
 @NgModule({
   declarations: [
@@ -35,10 +32,7 @@ import { Router } from '@angular/router';
     FormsModule,
     HttpModule,
     AppRouting,
-    MdButtonModule.forRoot(),
-    MdCoreModule.forRoot(),
-    MdCardModule.forRoot(),
-    MdInputModule.forRoot()
+    NglModule
   ],
   providers: [
     {provide: Http, useClass: HttpProvider, deps: [XHRBackend, RequestOptions, StorageService, Router]},
