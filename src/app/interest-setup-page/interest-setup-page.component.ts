35f7fad0c2c1119f940ce46618eeb91ba80c0cab
charlieamer <amer_zavlan@live.com>
2016-11-04 14:44:40 +0100

    Interest setup page selector

--- src/app/interest-setup-page/interest-setup-page.component.ts
+++ src/app/interest-setup-page/interest-setup-page.component.ts
@@ -12,6 +12,8 @@ export class InterestSetupPageComponent implements OnInit {
   category: FOSModel;
   root: FOSModel;
 
+  interests: InterestModel[] = [];
+
   constructor(protected storageService: StorageService) { }
 
   ngOnInit() {
@@ -19,6 +21,39 @@ export class InterestSetupPageComponent implements OnInit {
       this.category = root.children[2];
       console.log(this.category);
     });
+  }
+
+  findInterestByFos(fos: FOSModel): InterestModel {
+    return _.find(this.interests, (interest: InterestModel) => {
+      return interest.fos.code == fos.code;
+    })
+  }
+
+  isRemovable = (fos: FOSModel): boolean => { 
+    return this.findInterestByFos(fos) != null;
+  }
+
+  fosAdded(fos: FOSModel) {
+    if (this.isRemovable(fos)) {
+      console.error("${fos.name} was already added");
+    } else {
+      this.interests.push({
+        fos: fos,
+        fosId: fos.coreLookupId,
+        stars: 0
+      });
+    }
+    console.log(this.interests);
+  }
+
+  fosRemoved(fos: FOSModel) {
+    if (!this.isRemovable(fos)) {
+      console.error("${fos.name} wasn't added, but tried to remove it");
+    } else {
+      let toDelete = this.findInterestByFos(fos);
+      _.remove(this.interests, (f) => {return toDelete === f});
+    }
+    console.log(this.interests);
   }
 
 }
