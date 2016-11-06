94ba7ea31c3688c71e723d838d334f62524f9ce5
Ahmed Karic <Ahmed Karic>
2016-11-06 07:50:02 +0100

    setup choice component

--- src/app/interest-setup-choice/interest-setup-choice.component.ts
+++ src/app/interest-setup-choice/interest-setup-choice.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { InterestModel } from '../models/dto';
 
 @Component({
   selector: 'app-interest-setup-choice',
@@ -8,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class InterestSetupChoiceComponent implements OnInit {
 
   constructor() { }
-
+@Input() interest: InterestModel = { fosId:0, fos:{name:"MY CHOICE No1",children:[], parentFosId:-1,coreLookupId:0, code:"testni primjer"},stars:0};
+  niz: string[] = ["interest-setup:very low", "interest-setup:low", "interest-setup:good", "interest-setup:very good", "interest-setup:excellent"];  
   ngOnInit() {
   }
 
