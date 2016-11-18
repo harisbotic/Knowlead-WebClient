865b85d61d5389c03e1efdde359d4262fb21e425
charlieamer <amer_zavlan@live.com>
2016-11-18 16:56:21 +0100

    Login page converted to model driven form. Updated error component, added form control input. Translation for required field.

--- src/app/components/error-list/error-list.component.ts
+++ src/app/components/error-list/error-list.component.ts
@@ -1,12 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, DoCheck } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-error-list',
   templateUrl: './error-list.component.html',
   styleUrls: ['./error-list.component.scss']
 })
-export class ErrorListComponent {
+export class ErrorListComponent implements DoCheck {
 
-  @Input() errors: string[];
+  display: string[];
+
+  refresh = () => {
+    this.display = [];
+    if (this._errors)
+      this.display = _.clone(this._errors);
+    if (this._formControl && this._formControl.dirty)
+      this.display = this.display.concat(Object.keys(this._formControl.errors || {}).map(c => "validation:" + c));
+  }
+
+  _errors: string[];
+  @Input() set errors(value: string[]) {
+    this._errors = value;
+    this.refresh();
+  }
+
+  _formControl: FormControl;
+
+  @Input() set formControlProperty(value: FormControl) {
+    this._formControl = value;
+    if (value)
+      value.registerOnChange(this.refresh);
+    this.refresh();
+  }
+  get Object() { return Object; };
+
+  ngDoCheck() {
+    this.refresh();
+  }
 
 }
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SessionService } from "../../services/session.service";
 import "rxjs/Rx"
 import { Router } from '@angular/router';
 import { ResponseModel, RegisterUserModel } from './../../models/dto';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -10,26 +11,33 @@ import { ResponseModel, RegisterUserModel } from './../../models/dto';
   styleUrls: ['./login-page.component.scss'],
   providers: []
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
 
   success: boolean = false;
   busy: boolean = false;
   response: ResponseModel;
-  cridentials: RegisterUserModel;
+  form: FormGroup;
 
   constructor(protected sessionService: SessionService, protected router: Router) {
-    this.cridentials = {email: "", password: ""};
   }
 
   loginClicked() {
+    console.log(this.form.controls['email'].errors);
     this.busy = true;
     delete this.response;
-    this.sessionService.login(this.cridentials).finally(() => {
+    this.sessionService.login(this.form.value).finally(() => {
       this.busy = false;
     }).subscribe(loginResponse => {
       this.router.navigate(["/home"]);
     }, (errorResponse) => {
       this.response = errorResponse;
     });
+  }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required])
+    })
   }
 }
