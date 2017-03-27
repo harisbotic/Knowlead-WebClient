4597ba057ffe7b4bc758d1a6df11e189f6e2f6f5
Amer Zavlan <amer_zavlan@live.com>
2017-03-27 10:00:04 +0200

    made gulp smarter

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 let pug_plugin_ng = require('pug-plugin-ng');
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
 var watch = require('gulp-watch');
 var pug = require('gulp-pug');
 var gulp_watch_pug = require('gulp-watch-pug');
@@ -9,8 +11,11 @@ let toSrc = gulp.dest((file) => file.base);
 
 gulp.task('default', () => {
   gulp.src('src/**/*.pug')
+      .pipe(plumber())
       .pipe(watch('src/**/*.pug'))
-      .pipe(gulp_watch_pug('src/**/*.pug', { delay: 100 }))
+      // .pipe(gulp_watch_pug('src/**/*.pug', { delay: 100 }))
       .pipe(pug(pug_opts))
+      .on('error', gutil.log)
+      .on('data', () => gutil.log(gutil.colors.green('Compiled successfuly!')))
       .pipe(toSrc);
 });
