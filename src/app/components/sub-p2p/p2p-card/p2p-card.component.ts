f7521f4d2714e9dc945d63d0bca584c8971f34cf
Amer Zavlan <amer_zavlan@live.com>
2017-04-06 12:30:59 +0200

    p2p card functionality p2p card icon coloring left sidebar functionality (except upcoming events)

--- src/app/components/sub-p2p/p2p-card/p2p-card.component.ts
+++ src/app/components/sub-p2p/p2p-card/p2p-card.component.ts
@@ -21,6 +21,12 @@ export class P2pCardComponent extends P2pComponent implements OnInit {
     return super.p2pId;
   }
 
+  static colorLevels = ['low', 'mediocre', 'high'];
+  deadlineColor: string;
+  fosColor: string;
+  priceColor: string;
+  languageColor: string;
+
   constructor(protected accountService: AccountService,
               protected storageService: StorageService,
               protected p2pService: P2pService,
@@ -31,5 +37,76 @@ export class P2pCardComponent extends P2pComponent implements OnInit {
               protected activatedRoute: ActivatedRoute) {
     super(accountService, storageService, p2pService, notificationService,
           modelUtilsService, realtimeService, router, activatedRoute);
+  }
+
+  calculateDeadlineColor() {
+    if (this.p2p && this.p2p.deadline) {
+      const today: number = new Date().getTime();
+      const deadline: number = this.p2p.deadline.getTime();
+      const days = 1000 * 60 * 60 * 24;
+      if (deadline - today > 5 * days) {
+        this.deadlineColor = P2pCardComponent.colorLevels[0];
+      }
+      if (deadline - today <= 5 * days) {
+        this.deadlineColor = P2pCardComponent.colorLevels[1];
+      }
+      if (deadline - today < 1 * days) {
+        this.deadlineColor = P2pCardComponent.colorLevels[2];
+      }
+    }
+  }
+
+  calculateFosColor() {
+    if (this.user && this.p2p) {
+      this.fosColor = P2pCardComponent.colorLevels[0];
+      for (let interest of this.user.interests) {
+        if (interest.fosId === this.p2p.fosId) {
+          if (interest.stars <= 3) {
+            this.fosColor = P2pCardComponent.colorLevels[1];
+          } else {
+            this.fosColor = P2pCardComponent.colorLevels[2];
+          }
+        }
+      }
+    }
+  }
+
+  calculatePriceColor() {
+    if (this.p2p) {
+      if (this.p2p.actualPrice > 40) {
+        this.priceColor = P2pCardComponent.colorLevels[2];
+      }
+      if (this.p2p.actualPrice <= 40) {
+        this.priceColor = P2pCardComponent.colorLevels[1];
+      }
+      if (this.p2p.actualPrice <= 20) {
+        this.priceColor = P2pCardComponent.colorLevels[0];
+      }
+    }
+  }
+
+  calculateLanguageColor() {
+    if (this.p2p && this.p2p.languages && this.user) {
+      this.languageColor = P2pCardComponent.colorLevels[0];
+      for (let language of this.p2p.languages) {
+        if (this.user.motherTongueId === language.coreLookupId) {
+          this.languageColor = P2pCardComponent.colorLevels[2];
+          return;
+        }
+        for (let speakLanguage of this.user.languages) {
+          if (language.coreLookupId === speakLanguage.coreLookupId) {
+            this.languageColor = P2pCardComponent.colorLevels[1];
+          }
+        }
+      }
+    }
+  }
+
+  refresh() {
+    super.refresh();
+    this.calculateDeadlineColor();
+    this.calculateFosColor();
+    this.calculatePriceColor();
+    this.calculateLanguageColor();
   }
 }
