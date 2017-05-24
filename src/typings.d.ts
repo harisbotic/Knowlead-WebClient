2c6b032718ed8c0c07d054bb1dfeb06137f5ef9d
Amer Zavlan <amer_zavlan@live.com>
2017-05-24 13:15:07 +0200

    typescript compiler h4ck1ng

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,8 +3,9 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 declare var System: any;
-declare var module: { id: string };
-declare var require: any;
+// declare var module: { id: string };
+// declare var require: any;
+// declare var NodeRequire: any;
 // declare type DataReceived = (data: any) => void;
 // declare type ErrorHandler = (e: any) => void;
 // declare type ConnectionClosed = (e?: any) => void;
