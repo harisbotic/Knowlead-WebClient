081790d7f3d25d00bbe32beb553ba113d2e47ed7
Amer Zavlan <amer_zavlan@live.com>
2017-06-11 19:21:48 +0200

    p2p create fix

--- src/app/components/sub-p2p/p2p-create/p2p-create.component.ts
+++ src/app/components/sub-p2p/p2p-create/p2p-create.component.ts
@@ -10,6 +10,7 @@ import { dateValidator } from '../../../validators/date.validator';
 import { BaseFormComponent } from '../../../base-form.component';
 import { addHoursToDate } from '../../../utils/index';
 import { Router } from '@angular/router';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-p2p-create',
@@ -128,6 +129,15 @@ export class P2pCreateComponent extends BaseFormComponent<P2PModel> implements O
     }, (err) => {
       this.notificationService.error('error creating p2p', err);
     }));
+  }
+
+  getValue(): P2PModel {
+    // clone value so that we don't mess with original one
+    const ret = super.getValue();
+    ret.blobs = _.cloneDeep(ret.blobs);
+    // remove all files which are undefined/null
+    ret.blobs = ret.blobs.filter(blob => blob);
+    return ret;
   }
 
 }
