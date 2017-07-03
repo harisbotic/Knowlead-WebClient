c4cb4cdd83fee22a19d2482bbdd1d25f46dc890f
Amer Zavlan <amer_zavlan@live.com>
2017-07-03 18:22:33 +0200

    basic attachments functionality and html/css

--- src/app/components/file-thumbnail/file-thumbnail.component.ts
+++ src/app/components/file-thumbnail/file-thumbnail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { _BlobModel } from '../../models/dto';
+import { ModelUtilsService } from '../../services/model-utils.service';
 
 @Component({
   selector: 'app-file-thumbnail',
@@ -9,10 +10,16 @@ import { _BlobModel } from '../../models/dto';
 export class FileThumbnailComponent implements OnInit {
 
   @Input() file: _BlobModel;
+  isImage: boolean;
+  backgroundUrl: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.isImage = this.file.blobType === 'Image';
+    if (this.isImage) {
+      this.backgroundUrl = ModelUtilsService.getImageBlobUrl(this.file.blobId);
+    }
   }
 
 }
